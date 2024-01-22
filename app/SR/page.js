f2b958950a2838911ae1b3d0ce96901fdff27fa7
joroinnoroff <oinojorgen@gmail.com>
2024-01-22 16:26:07 -0600

    final

--- app/SR/page.js
+++ app/SR/page.js
@@ -63,7 +63,7 @@ export default function SR() {
           
           <div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96 hover:scale-105 transition-all">
             <div class="p-10">
-              <Link href={""} target="_blank">
+              <Link href={"https://mentalhelse.no/fa-hjelp/foreldresupport/"} target="_blank">
                 <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
                   Foreldresupport
                 </h5>
